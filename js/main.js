bd4af93c189ca77e4fb02a18b9ccb0f02dc9e97b
your_username <ayodejifasesin4505@gmail.com>
2022-04-29 07:38:45 -0500

    added blog page and login page

--- js/main.js
+++ js/main.js
@@ -6,6 +6,21 @@ fetch("https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=5f0b401b87
         document.querySelector('.news-body').innerText = data.articles[0].content
         document.querySelector('.news-img').src = data.articles[0].urlToImage
         document.querySelector('.news-link').href = data.articles[0].url
+
+        document.querySelector('.lead2').innerText = data.articles[1].title
+        document.querySelector('.news-body2').innerText = data.articles[1].content
+        document.querySelector('.news-img2').src = data.articles[1].urlToImage
+        document.querySelector('.news-link2').href = data.articles[1].url
+
+        document.querySelector('.lead3').innerText = data.articles[2].title
+        document.querySelector('.news-body3').innerText = data.articles[2].content
+        document.querySelector('.news-img3').src = data.articles[2].urlToImage
+        document.querySelector('.news-link3').href = data.articles[2].url
+
+        document.querySelector('lead4').innerText = data.articles[4].title
+        document.querySelector('news-body4').innerText = data.articles[4].content
+        document.querySelector('.news-img4').src = data.articles[4].urlToImage
+        document.querySelector('.news-link4').href = data.articles[4].url
        
         
     })
